3e66716453391e600024a2f13859d32d246e3e23
Faiz-git99 <marifaiz97@gmail.com>
2024-11-15 11:34:09 +0300

    module server

---
+++ app.js
@@ -0,0 +1,58 @@
+// importer le framework Express.js
+// import express from "express";
+const express = require("express");
+//crée l'application express.js dans la variable app
+const app = express();
+
+/* le serveur retourne la réponse ci-dessous peu importe la requête
+app.use((req, res) => {
+    res.json({message : "Votre message a bien été reçu"});
+}) */
+
+
+
+
+// on va définir une route de type GET
+app.get("/", (req, res) => {
+    res.end("Bonjour, dagoni woho");
+});
+
+app.get("/accueil", (req, res) => {
+    res.end("Bienvenue à l'acceuil");
+});
+
+app.get("/apropos", (req, res) => {
+    res.end("Qui sommes-nous");
+});
+
+app.get("/services", (req, res) => {
+    res.end("Les propositions");
+});
+
+app.get("/contact", (req, res) => {
+    res.end("Le formulaire à remplir");
+});
+
+// créer une route avec la methode POST
+app.post("/formulaire", (req, res) => {
+    res.end("Ton formulaire doit être à jour");
+});
+
+// créer une route avec la méthode PUT qui permet de modifier 
+app.put("/accueil", (req, res) => {
+    res.end("Welcome to my home");
+});
+
+
+// créer une route avec la méthode DELETE qui permet de supprime une route
+app.delete("/services", (req, res) => {
+    res.end("On a supprimé l'onglet services");
+});
+
+/*
+//lancer l'application
+app.listen(3001, () => {
+    console.log("Serveur écoute le port 3001");
+});
+*/
+module.exports = app ;
---
+++ server.js
@@ -0,0 +1,37 @@
+// Importe le protocole htpp
+// On peut l'écrire de façon suivante aussi import {createServer} from "http"
+const http = require('http');
+
+const app = require('./app');
+
+const numPort = 3002;
+
+app.set("port", numPort);
+
+const server = http.createServer(app);
+
+// déclaration du serveur en utilsant http
+// Il y a cette façon comme ça const server = http.createServer((req, res) => {});
+//const server = http.createServer((req, res) => {
+   // res.end("Bonjour, je suis le serveur. "); // res suivi de la methode .end() permet de retourner le message affiché
+//});
+
+const date = new Date();
+const time = date.toLocaleTimeString();
+ 
+// Le serveur est disponible sur le port 3000
+server.listen(numPort, () => {
+    console.log(date.toLocaleDateString(), date.toLocaleTimeString(), " - Le serveur est activé au port : ", numPort);
+}); // listen() veut dire "écoute" permet d'indiquer le lieu de l'affichage
+
+
+
+// importer le serveur app
+//const app = require("./app");
+
+// préciser l'entrée
+//app.set("port", 3000);
+
+//création du serveur
+//const server = createServer(app);
+
